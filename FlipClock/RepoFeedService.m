4f0c310840bfc3b6e8a1b237e6cb644f65973244
Jonathan Salvador <3270735+Goondaba@users.noreply.github.com>
2017-02-15 22:53:50 -0800

    impl basic feed fetch

--- FlipClock/RepoFeedService.m
+++ FlipClock/RepoFeedService.m
@@ -7,14 +7,64 @@
 //
 
 #import "RepoFeedService.h"
+#import <MWFeedParser/MWFeedParser.h>
+#import <MWFeedParser/NSString+HTML.h>
+
+static NSString * const kRepoURL = @"https://github.com/Goondaba/flipclock-screensaver/releases.atom";
+
+@interface RepoFeedService () <MWFeedParserDelegate>
+@property (nonatomic, strong) MWFeedParser *feedParser;
+@property (copy) newReleaseFetchCompletionBlock completionCallback;
+@property (nonatomic, strong) NSMutableArray *titleList;
+@end
 
 @implementation RepoFeedService
 
+- (id)init {
+    
+    if (self = [super init]) {
+        self.titleList = [NSMutableArray new];
+    }
+    return self;
+}
 
 - (void)newReleaseIsAvailable:(void (^) (BOOL newReleaseAvailable))completion {
     
+    if (!self.feedParser) {
+        NSURL *feedURL = [NSURL URLWithString:kRepoURL];
+        self.feedParser = [[MWFeedParser alloc] initWithFeedURL:feedURL];
+    }
+    
+    //prepare
+    [self.feedParser stopParsing];
+    [self.titleList removeAllObjects];
+    
+    self.feedParser.delegate = self;
+    self.feedParser.feedParseType = ParseTypeItemsOnly;
+    self.feedParser.connectionType = ConnectionTypeAsynchronously;
+    
+    self.completionCallback = completion;
+    [self.feedParser parse];
+}
 
-    //TODO: Do setup, fetch
+#pragma mark - MWFeedParser Delegate
+
+- (void)feedParser:(MWFeedParser *)parser didParseFeedItem:(MWFeedItem *)item {
+    
+    NSString *title = item.title;
+    if (title) {
+        [self.titleList addObject:[title stringByRemovingNewLinesAndWhitespace]];
+    }
+}
+
+- (void)feedParser:(MWFeedParser *)parser didFailWithError:(NSError *)error {
+    //TODO: error handling
+}
+
+- (void)feedParserDidFinish:(MWFeedParser *)parser {
+    
+    //TODO: Trigger callback
+    NSLog(@"%@", self.titleList);
 }
 
 
