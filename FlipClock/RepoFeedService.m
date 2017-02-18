ec5da0f3441b7eef7337c98cb109e1766a383f82
Jonathan Salvador <jtsalvad@gmail.com>
2017-02-17 22:01:47 -0800

    hooked up regex and callback

--- FlipClock/RepoFeedService.m
+++ FlipClock/RepoFeedService.m
@@ -13,10 +13,16 @@
 
 static NSString * const kRepoURL = @"https://github.com/Goondaba/flipclock-screensaver/releases.atom";
 
+typedef NS_ENUM(NSInteger, VersionRegexGroupSequence) {
+    VersionRegexGroupSequenceName = 1,
+    VersionRegexGroupSequenceVersion,
+};
+
+
 @interface RepoFeedService () <MWFeedParserDelegate>
 @property (nonatomic, strong) MWFeedParser *feedParser;
 @property (copy) newReleaseFetchCompletionBlock completionCallback;
-@property (nonatomic, strong) NSMutableArray *titleList;
+@property (nonatomic, strong) NSMutableArray<NSString *> *titleList;
 @end
 
 @implementation RepoFeedService
@@ -64,13 +70,43 @@ static NSString * const kRepoURL = @"https://github.com/Goondaba/flipclock-scree
 
 - (void)feedParserDidFinish:(MWFeedParser *)parser {
     
-    //Grab current version
+    if (self.titleList.count > 0) {
+        
+        NSString *latestVersion = [self latestVersionStringFromTitleString:[self.titleList firstObject]];
+        if (latestVersion) {
+            
+            //Grab current version
+            NSString *currentVersion = [NSString stringWithFormat:@"%@",[[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"]];
+            
+            self.completionCallback ([latestVersion isNewerThanVersion:currentVersion]);
+            return;
+        }
+    }
     
-    //Parse out numerical portion of title
-    
-    //TODO: Trigger callback
-    NSLog(@"%@", self.titleList);
+    self.completionCallback(NO);
 }
 
+#pragma mark - Custom 
 
+- (NSString *)latestVersionStringFromTitleString:(NSString *)string {
+    
+    NSError *error = NULL;
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"^\\s*(\\w+)\\s+(\\S+)"
+                                                                           options:NSRegularExpressionCaseInsensitive
+                                                                             error:&error];
+    
+    NSArray *matches = [regex matchesInString:string
+                                      options:0
+                                        range:NSMakeRange(0, [string length])];
+    for (NSTextCheckingResult *match in matches) {
+        
+        @try {
+            return [string substringWithRange:[match rangeAtIndex:VersionRegexGroupSequenceVersion]];
+        } @catch (NSException *exception) {
+            return nil;
+        }
+    }
+    
+    return nil;
+}
 @end
