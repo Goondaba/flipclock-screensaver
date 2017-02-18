7ac89abc3a3c9fb2c9f433065563675839e9463c
Jonathan Salvador <3270735+Goondaba@users.noreply.github.com>
2017-02-18 00:49:07 -0800

    now loading images from disk

--- FlipClock/TextureProviderService.m
+++ FlipClock/TextureProviderService.m
@@ -7,9 +7,10 @@
 //
 
 #import "TextureProviderService.h"
+#import "DigitNodeTextureNameUtil.h"
 
 @interface TextureProviderService ()
-@property (nonatomic, strong) NSDictionary<NSString *, NSImage*> *textures;
+@property (nonatomic, strong) NSMutableDictionary<NSString *, NSImage*> *textures;
 @end
 
 @implementation TextureProviderService
@@ -19,9 +20,26 @@
 }
 
 - (void)loadTexturesWithFontType:(DigitFontType)fontType {
+
+    if (!self.textures) {
+        self.textures = [NSMutableDictionary dictionary];
+    }
     
-    //TODO: Move to pre-built images for efficiency
-    self.textures = [DigitNodeImageGeneratorUtil generateTexturesWithFontType:fontType];
+    for(int i=0; i < numDigitType; i++){
+        
+        NSString *currentPrefix = [DigitNodeTextureNameUtil getTexturePrefixFor:i];
+        
+        //name to save resources under
+        NSString *top_str       = [NSString stringWithFormat:@"%@_top", currentPrefix];
+        NSString *bottom_str    = [NSString stringWithFormat:@"%@_bot", currentPrefix];
+        
+        //name to use to load image resources
+        NSString *topLoadName    = [NSString stringWithFormat:@"%@_%ld", top_str, (long)fontType];
+        NSString *bottomLoadName = [NSString stringWithFormat:@"%@_%ld", bottom_str, (long)fontType];
+        
+        [self.textures setValue:[NSImage imageNamed:topLoadName]    forKey:top_str];
+        [self.textures setValue:[NSImage imageNamed:bottomLoadName] forKey:bottom_str];
+    }
 }
 
 
